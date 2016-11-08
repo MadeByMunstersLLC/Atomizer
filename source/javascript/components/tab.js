2746eb639c44c41931b235747fb689671ddbdb77
kurtcunningham <cunningham.kurt@gmail.com>
2016-11-07 19:20:24 -0600

    added topbar

---
+++ source/javascript/components/tab.js
@@ -0,0 +1,20 @@
+// Tabs Component Interaction
+var $tabLink = $('.tab__item-link');
+
+$(document).ready(function() {
+  $tabLink.on('click', function(e)  {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var currentAttrValue = $(this).attr('href'),
+        $tabParent = $('.tab__item'),
+        activeClass = 'is-active';
+
+    // Show/Hide Tabs
+    $('.tabs ' + currentAttrValue).show().siblings().hide();
+
+    // Change/remove current tab to active
+    $(this).parent($tabParent).addClass(activeClass).siblings().removeClass(activeClass);
+
+  });
+});
